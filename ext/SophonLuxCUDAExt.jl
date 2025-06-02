7dd4f3b274a7b3a0490dc413ae19e7ea5c43a2b4
Henry Ruben Fischer <henryrubenfischer@gmail.com>
2025-06-02 17:11:11 +0200

    Try and update to Lux v1
    And other more current package versions

--- ext/SophonLuxCUDAExt.jl
+++ ext/SophonLuxCUDAExt.jl
@@ -2,7 +2,7 @@ module SophonLuxCUDAExt
 
 using Lux, LuxCUDA, Sophon, Optimization, Adapt
 
-function (::LuxCUDADevice)(prob::OptimizationProblem)
+function (:CUDADevice)(prob::OptimizationProblem)
     u0 = adapt(CuArray, prob.u0)
     p = Tuple(adapt(CuArray, prob.p[i]) for i in 1:length(prob.p))  # have to use tuple here...
     return Optimization.OptimizationProblem(prob.f, u0, p)
