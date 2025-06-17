6d487e0842fc05ce4635ef14975f844c010ff2a1
Henry Ruben Fischer <henryruben.fischer@stud.uni-goettingen.de>
2025-06-17 17:02:23 +0200

    Hopefully fix CudaDevice callable

--- ext/SophonLuxCUDAExt.jl
+++ ext/SophonLuxCUDAExt.jl
@@ -2,7 +2,7 @@ module SophonLuxCUDAExt
 
 using Lux, MLDataDevices,  Sophon, Optimization, Adapt
 
-function (:CUDADevice)(prob::OptimizationProblem)
+function (::MLDataDevices.CUDADevice)(prob::OptimizationProblem)
     u0 = adapt(CuArray, prob.u0)
     p = Tuple(adapt(CuArray, prob.p[i]) for i in 1:length(prob.p))  # have to use tuple here...
     return Optimization.OptimizationProblem(prob.f, u0, p)
